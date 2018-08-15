936aadb0a916bfbec9063eeb8acf93d262268df0
quietlurker <ghosia.spam@gmail.com>
2018-08-15 07:37:32 +0200

    "first commit"

---
+++ konsola.py
@@ -0,0 +1,11 @@
+wzrost = 1.73
+waga = 66.3
+print("Moje BMI wynosi", waga/wzrost**2)
+print("Zapotrzebowanie kaloryczne")
+print("Wzór: 10 x masa ciała + 6.25 x wzrost w cm – 5 x wiek + S")
+print("współczynnik S: dla kobiet = -161, dla mężczyzn= +5")
+wzrost_cm = 170
+print("Moje zapotrzebowanie kaloryczne wynosi ", 10*waga+ 6.25*wzrost_cm - 5*39 -161)
+zapotrzenowanie = (10*waga)+ (6.25*wzrost_cm) - (5*39) -161
+print (zapotrzenowanie*1.6)
+
